07ad20c8d6c972a2ab957acdf4d84ee6cdb38af2
Julien Folliot <1753578+JulienFolliot@users.noreply.github.com>
2024-11-11 12:34:48 +0100

    Base archi v0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+# 11ty Starter
+
+## Introduction
+
+Starter project 11ty.
+
+### To start watching
+```
+npm start
+```
+
+## Checklist
+
+- 1. Mettre à jour le fichier src/head/meta
+- 2. Copier le logo en svg dans `src/assets/svg/misc/logo.svg`
+- 3. Faire un `npm run favicons` pour générer les favicons
+- 4. Mettre l'image par défaut pour le partage de lien au format 16/9 dans `src/assets/images/template/opengraph-default.jpg`
+- 
---
+++ src/_data/meta.js
@@ -0,0 +1,17 @@
+export const url = process.env.URL || 'http://localhost:8080';
+export const siteName = '11ty-Starter';
+export const siteDescription = 'A custom starter kit for 11ty website projects';
+export const siteType = 'Person';
+export const locale = 'fr_FR';
+export const lang = 'fr';
+export const creator = {
+    name: 'Julien Folliot', // i.e. Lene Saile - creator's (developer) name.
+    email: 'hello@julien-folliot',
+    website: 'https://www.julien-folliot.fr',
+    social: 'https://www.linkedin.com/in/julien-folliot-dev/'
+};
+export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
+export const themeColor = '#DD4462'; //  Manifest: defines the default theme color for the application
+export const themeBgColor = '#FBFBFB'; // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
+export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
+export const opengraph_default_alt = siteDescription; // alt text for default meta image"
